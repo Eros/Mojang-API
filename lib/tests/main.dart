1a0f4cb3e2376f2e45b1657ca14ba870819c0464
gp2-marr <george.marr.cobc@gmail.com>
2020-08-01 20:46:25 +0100

    implement more tests

--- lib/tests/main.dart
+++ lib/tests/main.dart
@@ -1,7 +1,10 @@
 
 import 'package:mojang_api/MojangAPI.dart';
+import 'package:mojang_api/MojangStatusAPI.dart';
 
 void main() async {
+
+  //main mojang api for players
   print(await MojangAPI.uuidForName("RapidTheNerd"));
   print(await MojangAPI.uuidForNameAt("RapidTheNerd", DateTime.now().millisecondsSinceEpoch.toString()));
   print(await MojangAPI.uuidForNames("RapidTheNerd"));
@@ -9,4 +12,11 @@ void main() async {
   print(await MojangAPI.nameHistoryForUuid("43db704e-10b1-40b3-a38d-ce059de35a59"));
   print(await MojangAPI.nameHistoryForName("RapidTheNerd"));
   print(await MojangAPI.profileForUUid("43db704e-10b1-40b3-a38d-ce059de35a59"));
+
+  //status api
+  print(await MojangStatusAPI.getApiStatus());
+  print(await MojangStatusAPI.getAuthStatus());
+  print(await MojangStatusAPI.getSessionStatus());
+  print(await MojangStatusAPI.getSkinsStatus());
+  print(await MojangStatusAPI.getWebsiteStatus());
 }
